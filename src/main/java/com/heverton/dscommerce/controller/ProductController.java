a9e4a6dd8f1a5305eef8df3ba9b76b967e1b3354
hevertonxav <hevertonxs@gmail.com>
2026-05-31 20:03:45 -0300

    Get Products paged

--- src/main/java/com/heverton/dscommerce/controller/ProductController.java
+++ src/main/java/com/heverton/dscommerce/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.heverton.dscommerce.controller;
 
 import com.heverton.dscommerce.dto.ProductDTO;
-import com.heverton.dscommerce.entities.Product;
-import com.heverton.dscommerce.repositories.ProductRepository;
 import com.heverton.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,6 +23,11 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id){
         return service.findByd(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 
 
--- src/main/java/com/heverton/dscommerce/services/ProductService.java
+++ src/main/java/com/heverton/dscommerce/services/ProductService.java
@@ -4,10 +4,13 @@ import com.heverton.dscommerce.dto.ProductDTO;
 import com.heverton.dscommerce.entities.Product;
 import com.heverton.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
 public class ProductService {
@@ -21,4 +24,12 @@ public class ProductService {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
 }
