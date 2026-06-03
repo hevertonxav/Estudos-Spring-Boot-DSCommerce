58698ffaa5a3159a450e347f66f99cd42adba144
hevertonxav <hevertonxs@gmail.com>
2026-06-02 23:11:02 -0300

    Bean Validation

--- src/main/java/com/heverton/dscommerce/controller/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/heverton/dscommerce/controller/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.heverton.dscommerce.controller.handlers;
 
 import com.heverton.dscommerce.dto.CustomError;
+import com.heverton.dscommerce.dto.ValidationError;
 import com.heverton.dscommerce.services.exceptions.DatabaseException;
 import com.heverton.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -44,6 +47,27 @@ public class ControllerExceptionHandler {
                 e.getMessage(),
                 request.getRequestURI()
         );
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError err = new ValidationError(
+                Instant.now(),
+                status.value(),
+                "Dados inválidos",
+                request.getRequestURI()
+        );
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
 
         return ResponseEntity.status(status).body(err);
     }
